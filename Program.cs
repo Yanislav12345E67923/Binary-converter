2ac77907001dbced189a9a6793be1f19fe777047
Yanislav12345E67923 <yanislav1234567@gmail.com>
2023-06-20 20:39:33 +0300

    Project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _Binary_converter;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the image address: ");
+        string address = Console.ReadLine();
+
+        List<string> output = new List<string>();
+        for (int i = 0; i < address.Length; i++)
+        {
+            output.Add(Convert.ToString((int)address[i], 2));
+        }
+        Console.Write("The address converted into binary code -> ");        
+        Console.WriteLine(string.Join("", output));
+    }
+}
+
